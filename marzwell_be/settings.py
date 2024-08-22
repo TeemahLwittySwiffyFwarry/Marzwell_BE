9a16c1212aa32c6b16a63b4f2b553fcb54b74472
TeemahLwittySwiffyFwarry <98737888+TeemahLwittySwiffyFwarry@users.noreply.github.com>
2024-08-23 00:12:59 +0100

    Updated python decouple

--- marzwell_be/settings.py
+++ marzwell_be/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -141,10 +142,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # settings.py
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.google.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'irecruitmentmain@gmail.com'
-EMAIL_HOST_PASSWORD = 'Yemmy@256'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 
