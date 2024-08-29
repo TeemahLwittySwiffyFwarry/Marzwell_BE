eea05b605fc5b9939f6ee18ba2114f4e8fe2cbc6
TeemahLwittySwiffyFwarry <98737888+TeemahLwittySwiffyFwarry@users.noreply.github.com>
2024-08-29 04:44:35 +0100

    Add approval to the serializer

--- testimonial/admin.py
+++ testimonial/admin.py
@@ -3,7 +3,7 @@ from .models import Testimonial
 
 # Define a ModelAdmin class to customize the admin interface
 class TestimonialAdmin(admin.ModelAdmin):
-    list_display = ("name", "relationship_to_school", "testimonial")
+    list_display = ("name", "relationship_to_school", "testimonial", "approval")
 
 # Register the model and its ModelAdmin class
 admin.site.register(Testimonial, TestimonialAdmin)
--- testimonial/serializers.py
+++ testimonial/serializers.py
@@ -4,5 +4,5 @@ from .models import Testimonial
 class TestimonialSerializer(serializers.ModelSerializer):
     class Meta:
         model = Testimonial
-        fields = ['id', 'name', 'relationship_to_school', 'testimonial', 'image', 'date_created', 'rating']
+        fields = ['id', 'name', 'relationship_to_school', 'testimonial', 'image', 'date_created', 'rating', 'approval']
         read_only_fields = ['date_created']
